b856270d865e61b5f6a70ce21f1e0c79366eff63
jsRuiner <mega.vzlomer111@gmail.com>
2021-09-22 21:01:46 +0300

    create output on the page

--- server.go
+++ server.go
@@ -1,28 +1,34 @@
 package main
+
 import (
-    "time"
+    "encoding/json"
+    "fmt"
     "log"
     "net/http" // HTTP protocol package
-    "encoding/json"
+    "time"
 )
- 
-func TimeResponseHandler(w http.ResponseWriter, r *http.Request) {
-    test := &Time{Time: time.Now().Format(time.RFC3339)} // get current time with our format   
-    res := json.Marshal(test)
-    w.Header().Set("content-type", "aplication/json")
-    w.WriteHeader(200)
-    w.Write(res)
+
+func TimeResponseHandler(w http.ResponseWriter, req *http.Request) {
+    test := &Time{Time: time.Now().Format(time.RFC3339)} // get current time with our format
+    res, _ := json.Marshal(test)
+
+    fmt.Fprintf(w, "%s\n", res)
+    //w.Header().Set("content-type", "aplication/json")
+    //w.WriteHeader(200)
+    //w.Write(res)
+}
+
+type Time struct {
+    Time string json:"time"
 }
 
 func main() {
-    type Time struct {
-        Time string `json:"time"`
+    http.HandleFunc("/time", TimeResponseHandler)
+    log.Println("Starting HTTP server")
+    err := http.ListenAndServe(":3000", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
     }
-    http.HandleFunc("/time", TimeResponseHandler) {
-        log.Println("Starting HTTP server")
-        err := http.ListenAndServe(":3000", nil)
-        if err != nil {
-            log.Fatal("ListenAndServe: ", err)
-        }
-    }
+
 }
+
