46c9fed340e6966315f152433fede4e0e9867b53
jsRuiner <mega.vzlomer111@gmail.com>
2021-09-22 19:59:27 +0300

    Add port and create http server

--- server.go
+++ server.go
@@ -1,18 +1,28 @@
 package main
 import (
     "time"
-    "os" // file usage package
-    "net/http" // HTTP protocol package
     "log"
+    "net/http" // HTTP protocol package
+    "encoding/json"
 )
  
 func TimeResponseHandler(w http.ResponseWriter, r *http.Request) {
     test := &Time{Time: time.Now().Format(time.RFC3339)} // get current time with our format   
+    res := json.Marshal(test)
+    w.Header().Set("content-type", "aplication/json")
+    w.WriteHeader(200)
+    w.Write(res)
 }
+
 func main() {
     type Time struct {
         Time string `json:"time"`
     }
-    http.HandleFunc("/time",)
-    
+    http.HandleFunc("/time", TimeResponseHandler) {
+        log.Println("Starting HTTP server")
+        err := http.ListenAndServe(":3000", nil)
+        if err != nil {
+            log.Fatal("ListenAndServe: ", err)
+        }
+    }
 }
