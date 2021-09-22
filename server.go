8f91631f2c3828afb29d7901646112e042cc49fc
ddynikov <ddynikov@gmail.com>
2021-09-22 23:47:33 +0300

    tilted

--- server.go
+++ server.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http" // HTTP protocol package
 	"time"
 )
 
 func TimeResponseHandler(w http.ResponseWriter, req *http.Request) {
-	test := &Time{Time: time.Now().Format(time.RFC3339)} // get current time with our format
-	res, _ := json.Marshal(test)
+	output := &Time{Time: time.Now().Format(time.RFC3339)} // get current time with our format
+	res, _ := json.Marshal(output)
 
-	log.Println(w, "%s\n", res)
+	fmt.Fprintf(w, "%s\n", res)
 	//w.Header().Set("content-type", "aplication/json") // downloading page content
 	//w.WriteHeader(200)
 	//w.Write(res)
@@ -22,7 +23,7 @@ type Time struct {
 }
 
 func main() {
-	log.Println("HTTP server started")
+	log.Printf("HTTP server started")
 	http.HandleFunc("/time", TimeResponseHandler)
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
