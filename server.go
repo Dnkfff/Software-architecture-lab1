c1258ea906d1eb1130a90196949d6eeefd825a8a
jsRuiner <mega.vzlomer111@gmail.com>
2021-09-22 19:09:07 +0300

    Create Time structure, add sm packages

--- server.go
+++ server.go
@@ -1,9 +1,16 @@
 package main
 import (
-    "fmt"
-    "os"
+    "time"
+    "os" // file usage package
+    "fmt" // formatted input output package
+    "net/http" // HTTP protocol package
+    "log"
 )
  
+
 func main() {
+    type Time struct {
+        Time string `json:"time"`
+    }
     fmt.Fprintln(os.Stdout, "hello cold")
 }
