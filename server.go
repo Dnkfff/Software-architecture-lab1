641d5afc61c505f9ea6407da9511095ffbb93fef
ddynikov <ddynikov@gmail.com>
2021-09-22 20:21:54 +0300

    Syntax mistakes correct

--- server.go
+++ server.go
@@ -1,9 +1,30 @@
 package main
+
 import (
-    "fmt"
-    "os"
+	"encoding/json"
+	"log"
+	"net/http" // HTTP protocol package
+	"time"
 )
- 
+
+func TimeResponseHandler(w http.ResponseWriter, req *http.Request) {
+	test := &Time{Time: time.Now().Format(time.RFC3339)} // get current time with our format
+	res, _ := json.Marshal(test)
+	w.Header().Set("content-type", "aplication/json")
+	w.WriteHeader(200)
+	w.Write(res)
+}
+
+type Time struct {
+	Time string `json:"time"`
+}
+
 func main() {
-    fmt.Fprintln(os.Stdout, "hello cold")
+	http.HandleFunc("/time", TimeResponseHandler)
+	log.Println("Starting HTTP server")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
 }
